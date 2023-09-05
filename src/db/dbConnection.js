1522c6535402bf4d31b64f7b03eecfe323e2edf5
Rose1845 <odhiamborose466@gmail.com>
2023-09-05 18:25:56 +0300

    delete unnecesary files

--- src/db/dbConnection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose
-  .connect(
-    "mongodb+srv://dev_rose:Atieno_20.raocluster.oautq.mongodb.net/bookStore?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("connected to db"))
-  .catch((error) => console.log(error));
